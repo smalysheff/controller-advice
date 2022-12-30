94903a6f8c49561c5049b72abe92c322eaa72691
Smal <malyshevs_v@mail.rul>
2022-12-31 01:16:28 +0300

    added properties

--- src/main/java/ru/smal/exception/ExceptionControllerAdvice.java
+++ src/main/java/ru/smal/exception/ExceptionControllerAdvice.java
@@ -1,15 +1,19 @@
 package ru.smal.exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 
 @RestControllerAdvice
-public class ExceptionControllerAdvice {
+public class ExceptionControllerAdvice extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(NoSuchElementFoundExeption.class)
     public ResponseEntity<ErrorDetail> handleNoSuchException(RuntimeException ex, WebRequest request) {
@@ -29,6 +33,20 @@ public class ExceptionControllerAdvice {
         ErrorDetail detail = new ErrorDetail();
         detail.setTimestamp(LocalDateTime.now());
         detail.setStatus(HttpStatus.BAD_REQUEST);
+        detail.setErrorDesc(ex.getMessage());
+        detail.setPath(request.getContextPath());
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(detail);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        ErrorDetail detail = new ErrorDetail();
+        detail.setTimestamp(LocalDateTime.now());
+        detail.setStatus(HttpStatus.NOT_FOUND);
         detail.setErrorDesc(ex.getMessage());
         detail.setPath(request.getContextPath());
 
